92c3f085b294c7e9c197b39e58dfe1ee55669f5e
Eshily Aguiar <eshiilyy@gmail.com>
2025-03-09 17:25:04 -0300

    feat: adicionar lista de números com 5 entradas do usuário
    O código solicita 5 números ao usuário via prompt, armazena esses números em uma lista (array) e exibe cada número no console.

---
+++ q4.js
@@ -0,0 +1,12 @@
+let lista = []
+
+for(let i = 0; i < 5; i++){
+    let numero = parseInt(prompt("Digite um numero: "))
+    lista.push(numero)
+}
+
+for(let i = 0; i <= lista.length; i ++){
+    console.log(lista[i])
+}
+
+
